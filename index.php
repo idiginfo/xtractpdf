bcd37e2efeff6106456b58941bb8d9c42a2acf0c
Casey McLaughlin <caseyamcl@gmail.com>
2013-06-19 14:52:04 -0400

    Updated index.php with error message in case composer isn't built

--- index.php
+++ index.php
@@ -4,6 +4,10 @@ namespace XtractPDF;
 
 // ------------------------------------------------------------------
 
+if ( ! is_readable(__DIR__ . '/app/vendor/autoload.php')) {
+    die("Incomplete installation");
+}
+
 require(__DIR__ . '/app/vendor/autoload.php');
 
 App::main(isset($dev) ? App::DEVELOP : App::PRODUCTION);
