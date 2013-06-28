4a919eae7a186d77a27806f85de96891950de0e4
Casey McLaughlin <caseyamcl@gmail.com>
2013-06-28 12:50:12 -0400

    Added license and copyright notice

--- app/src/XtractPDF/PdfDataHandler/PdfDataHandlerInterface.php
+++ app/src/XtractPDF/PdfDataHandler/PdfDataHandlerInterface.php
@@ -1,4 +1,19 @@
 <?php
+  
+/**
+ * XtractPDF - A PDF Content Extraction and Curation Tool
+ *
+ * This program is free software under the GNU General Public License (v2)
+ * See LICENSE.md for a complete copy of the license
+ *
+ * @package     XtractPDF
+ * @author      Florida State University iDigInfo (http://idiginfo.org)
+ * @copyright   Copyright (C) 2013 Florida State University (http://fsu.edu)
+ * @license     http://www.gnu.org/licenses/gpl-2.0.txt
+ * @link        http://idiginfo.org
+ */
+
+// ------------------------------------------------------------------
 
 namespace XtractPDF\PdfDataHandler;
 
